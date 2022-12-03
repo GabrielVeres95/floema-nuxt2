5144946c92acdd70b4a533b0f6b8b7ff7d4c2d6b
Gabriel Veres <veresgabriel95@gmail.com>
2022-12-03 14:10:46 +0200

    Slight fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,10 +7,11 @@ export default {
     charset: 'utf-8',
     viewport: 'width=device-width, initial-scale=1', 
     htmlAttrs: {
-      lang: "ro"
+      lang: "en"
     },
 
     meta: [
+      { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width,initial-scale=1.0' },
       // https://realfavicongenerator.net/
       { name: 'msapplication-TileColor', content: '#f4d8cc' },
